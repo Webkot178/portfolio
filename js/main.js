e420dd08233e625f56639e4067f1bd7f1e51d5e7
Oleg Portniagin <121032976+Webkot178@users.noreply.github.com>
2023-01-17 16:08:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// MobileNavigation
+
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav');
+const overlay = document.querySelectorAll('.overlay');
+
+navIcon.addEventListener('click', function () {
+	this.classList.toggle('nav-icon--active');
+	nav.classList.toggle('nav--active');
+	overlay.forEach(function (item) {
+		item.classList.toggle('overlay--active');
+	});
+});
+
+// Находим ссылки внутри мобильной навигации
+const navLinks = document.querySelectorAll('.nav a');
+
+// Обходим ссылки методом forEach
+navLinks.forEach(function (item) {
+	// Для каждой ссылки добавляем прослушку по событию "Клик"
+	item.addEventListener('click', function () {
+		navIcon.classList.remove('nav-icon--active'); // Убираем активный класс у иконки моб. навигации
+		nav.classList.remove('nav--active'); // Убираем активный класс у блока моб. навигации
+		overlay.forEach(function (item) {
+			item.classList.toggle('overlay--active');
+		});
+	})
+});
